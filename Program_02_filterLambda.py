88b311a7525f5bbd8c32fbab3481b2cc4c5d652d
shakib <shakib@gmail.com>
2023-07-05 12:25:37 +0530

    [del] Delete a Program_02 ...

--- Program_02_filterLambda.py
+++
@@ -1,7 +0,0 @@
-# Write program for filter even numbers from a list using lambda function with filter().
-# numbers = [22,35,98,32,41,7]
-# O/P: [22, 98, 32]
-
-firstList  = [22,35,98,32,41,7]
-output = filter(lambda number :number%2==0 ,firstList)
-print(list(output))
