afcbadd7cdedf7991c0aed7f40e5c5f35b6136ed
shakib <shakib@gmail.com>
2023-07-05 12:34:01 +0530

    [update] Python file updated ...

--- Program_10_stringChecking.py
+++ Program_10_stringChecking.py
@@ -1,6 +1,6 @@
 # Write program check if string is a valid identifier or not
 # Note: For checking string use search() method of regex module.
-
+# you aare comments ..
 import re
 
 givenString = "This is a last Program"
