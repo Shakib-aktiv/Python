d58388d685fed41d6435553392c86dd9e27141a4
shakib <shakib@gmail.com>
2023-07-05 11:59:21 +0530

    [add] Python file added successffully ...

---
+++ Program_01_dictComp.py
@@ -0,0 +1,15 @@
+# Creating a new dictionary with only pairs where the key is any alphabet after "d" alphabet
+# (ex. e,f,g,h,i, etc..) and value is larger than 2 (Using Dictionary comprehension)
+# d = {'a':1,'e':2,'c':3, 'd':5, 'j':8}
+# O/p: {'j': 8}
+
+dictionary =  {'a':1,'e':4,'c':3, 'd':5, 'j':1, 'h':12}
+
+wlistofKeys  = list(dictionary.keys()) 
+listofKeys =sorted(wlistofKeys)
+dIndex= listofKeys.index('d')
+for i in range(dIndex+1,len(dictionary)):
+	if dictionary[listofKeys[i]]>2:
+		print({f'{listofKeys[i]}':dictionary[listofKeys[i]]})	 
+
+
